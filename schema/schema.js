9a811970707bc8aad22776d3c49475cfd0feca71
Carlos Calderon <cdcalderon@gmail.com>
2022-07-05 22:35:06 -0500

    complete schema

--- schema/schema.js
+++ schema/schema.js
@@ -9,6 +9,23 @@ const {
     GraphQLNonNull,
 } = graphql
 
+const CompanyType = new GraphQLObjectType({
+    name: "Company",
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parentValue, args) {
+                return axios
+                    .get(`http://localhost:3000/companies/${parentValue.id}/users`)
+                    .then((res) => res.data)
+            },
+        },
+    }),
+})
+
 const UserType = new GraphQLObjectType({
     name: "User",
     fields: () => ({
@@ -48,4 +65,37 @@ const RootQuery = new GraphQLObjectType({
             },
         },
     },
+})
+
+const mutation = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        addUser: {
+            type: UserType,
+            args: {
+                firstName: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: GraphQLInt },
+                companyId: { type: GraphQLString },
+            },
+            resolve(parentValue, { firstName, age, companyId }) {
+                return axios
+                    .post("http://localhost:3000/users", { firstName, age, companyId })
+                    .then((res) => res.data)
+            },
+        },
+        deleteUser: {
+            type: UserType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLString) },
+            },
+            resolve(parentValue, { id }) {
+                return axios.delete(`http://localhost:3000/users/${id}`).then((res) => res.data)
+            },
+        },
+    },
+})
+
+module.exports = new GraphQLSchema({
+    mutation,
+    query: RootQuery,
 })
